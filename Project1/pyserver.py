2513eeaa91d78f271c8ad0a50572a394cc02384a
aridasz <36234711+aridasz@users.noreply.github.com>
2018-02-08 20:18:58 -0500

    go

---
+++ Project1/pyserver.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+Very simple HTTP server in python for logging requests
+Usage::
+    ./server.py [<port>]
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import requests
+import json
+
+url = 'https://jsonplaceholder.typicode.com/posts'
+data = '''{
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "text": {
+            "record.document": "SOME_JOURNAL"
+          }
+        },
+        {
+          "text": {
+            "record.articleTitle": "farmers"
+          }
+        }
+      ],
+      "must_not": [],
+      "should": []
+    }
+  },
+  "from": 0,
+  "size": 50,
+  "sort": [],
+  "facets": {}
+}'''
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        response = requests.post(url, data=data)
+        logging.info(response.text)
+        self.wfile.write(response.text.encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        post_json = json.loads(post_data.decode('utf-8'))
+        logging.info(post_json['input'])
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=9070):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
