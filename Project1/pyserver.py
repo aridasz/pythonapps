aff0f55ccdf55e6d089bd74023f9db4aa6780b9a
aridasz <36234711+aridasz@users.noreply.github.com>
2018-02-09 20:51:39 -0500

    adding file serving capability
    added file serving

--- Project1/pyserver.py
+++ Project1/pyserver.py
@@ -8,6 +8,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import requests
 import json
+import os
 
 url = 'https://jsonplaceholder.typicode.com/posts'
 data = '''{
@@ -43,10 +44,32 @@ class S(BaseHTTPRequestHandler):
 
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        response = requests.post(url, data=data)
-        logging.info(response.text)
-        self.wfile.write(response.text.encode('utf-8'))
+        #self._set_response()
+
+        root = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'html')
+        # print(self.path)
+        if self.path == '/':
+            filename = root + '/index.html'
+        else:
+            filename = root + self.path
+
+        self.send_response(200)
+        if filename[-4:] == '.css':
+            self.send_header('Content-type', 'text/css')
+        elif filename[-5:] == '.json':
+            self.send_header('Content-type', 'application/javascript')
+        elif filename[-3:] == '.js':
+            self.send_header('Content-type', 'application/javascript')
+        elif filename[-4:] == '.ico':
+            self.send_header('Content-type', 'image/x-icon')
+        else:
+            self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as fh:
+            html = fh.read()
+            # html = bytes(html, 'utf8')
+            self.wfile.write(html)
+
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
@@ -55,9 +78,10 @@ class S(BaseHTTPRequestHandler):
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
         post_json = json.loads(post_data.decode('utf-8'))
         logging.info(post_json['input'])
+        response = requests.post(url, data=data)
+        logging.info(response.text)
+        self.wfile.write(response.text.encode('utf-8'))
 
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
 def run(server_class=HTTPServer, handler_class=S, port=9070):
     logging.basicConfig(level=logging.INFO)
